9034b98d83ed5f2f97ea6b24c1e17302d43dbd8a
TANCO12580 <2270364052@qq.com>
2021-06-25 19:48:58 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
 from flask import Flask
-app = Flask(__name__)#创建flask实例
+import os
+import sys
+import click
 from flask import url_for, escape
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+app = Flask(__name__)#创建flask实例
 
-# @app.route('/')
-# def hello():
-#     return 'Hello'
+#数据库配置
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+#创建数据库模型
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+#自定义函数创建数据库
+import click
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -22,24 +57,54 @@ def user_page(name):
 #     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
 #     return 'Test page'
 
-name = 'Grey Li'
+name = 'gks'
 movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
+    {'title': '机器学习讲座合集', 'year': '1988'},
+    {'title': '深度学习讲座合集', 'year': '1989'},
+    {'title': '机器视觉讲座合集', 'year': '1993'},
+    {'title': 'AI识别讲座合集', 'year': '1994'},
+    {'title': '数据挖掘讲座合集', 'year': '1996'},
+    {'title': '智能算法讲座合集', 'year': '1996'},
+    {'title': '模式识别讲座合集', 'year': '1999'},
+    {'title': '机器学习讲座合集', 'year': '1999'},
+    {'title': '机器学习讲座合集', 'year': '2008'},
+    {'title': '机器学习讲座合集', 'year': '2012'},
 ]
 
 from flask import Flask, render_template
 
-# ...
+#编写一个命令函数把虚拟数据添加到数据库里
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
